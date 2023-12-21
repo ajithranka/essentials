a264c78e22f0966b1edeeee85ca2326e5b083b71
Ajith Ranka <ajithranka@users.noreply.github.com>
2023-12-21 23:14:21 +0530

    generate css props and tailwind config from token json

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,26 @@
+const tokens = require('./app/styles/tokens.json')
+
+const colors = toTailwind(tokens, 'color')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{ts,tsx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      ...colors,
+    },
+  },
+}
+
+// Helpers
+
+function toTailwind(tokens, category) {
+  return Object.keys(tokens[category]).reduce((acc, name) => {
+    return {
+      ...acc,
+      [name]: `var(--${category}-${name})`,
+    }
+  }, {})
 }
